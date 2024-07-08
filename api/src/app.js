86cd303f947f6f748bca24a658503d08bde38d32
Maxi S <maxi0062009@hotmail.com>
2024-07-08 20:34:02 -0300

    modifico el verceljson y cambio ssl a true

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
   connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true//lo tenia en false
   }
 });
 
