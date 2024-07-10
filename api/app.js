d391e0a09579f6b61985e5d96c5e8dbd3bbd334d
Maxi S <maxi0062009@hotmail.com>
2024-07-09 23:10:21 -0300

    subo un nivel app.js

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { Pool } = require('pg');
-const corsOptions = require('../config/corsConfig');
+const corsOptions = require('./config/corsConfig');
 require('dotenv').config();
 
 const app = express();
