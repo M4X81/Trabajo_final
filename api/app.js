594ace545aaa239a787146a0b8b87dc04cbe0308
Maxi S <maxi0062009@hotmail.com>
2024-07-12 19:26:33 -0300

    hize built voy a ver que pasa

--- api/app.js
+++ api/app.js
@@ -78,78 +78,22 @@ app.post('/login', async (req, res) => {
 app.get('/users/:email', async (req, res) => {
     const { email } = req.params;
 
-    // try {
-    //     const userResult = await pool.query(
-    //         'SELECT id FROM user_profiles WHERE email = $1',
-    //         [email]
-    //     );
-
-    //     if (userResult.rows.length === 0) {
-    //         return res.status(404).json({ error: 'Usuario no encontrado' });
-    //     }
-
-    //     const userId = userResult.rows[0].id;
-    //     const profileResult = await pool.query(
-    //         'SELECT * FROM user_profiles WHERE user_id = $1',
-    //         [userId]
-    //     );
-
-    //     if (profileResult.rows.length === 0) {
-    //         return res.status(404).json({ error: 'Perfil de usuario no encontrado' });
-    //     }
-
-    //pruebo esto
-
     try {
-        const user = await getUserByEmail(email);
+        const user = await pool.query('SELECT * FROM user_profiles WHERE email = $1', [email]);
 
-        if (!user) {
+        if (user.rows.length === 0) {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
-        res.status(200).json(profileResult.rows[0]);
+
+        res.status(200).json(user.rows[0]);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
+
 //para modificar(agregar)
 // Ruta para actualizar datos del perfil del usuario
-// app.put('/users:email', async (req, res) => {
-// app.put('/users/:email', async (req, res) => { //ver bien esta ruta si es /users o /users/:email
-//     const { password, user_name, lastname, address, phone, country, city } = req.body;
-
-//     try {
-//         const userResult = await pool.query(
-//             'SELECT id FROM user_profiles WHERE email = $1',
-//             [email]
-//         );
-
-//         if (userResult.rows.length === 0) {
-//             return res.status(404).json({ error: 'Usuario no encontrado' });
-//         }
-
-//         const userId = userResult.rows[0].id;
-
-//         const profileResult = await pool.query(
-//             `INSERT INTO user_profiles (user_id, password, user_name, lastname, address, phone, country, city)
-//              VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-//              ON CONFLICT (user_id) DO UPDATE SET
-//              password = EXCLUDED.password,
-//              username = EXCLUDED.user_name,
-//              lastname = EXCLUDED.lastname,
-//              address = EXCLUDED.address,
-//              phone = EXCLUDED.phone,
-//              country = EXCLUDED.country,
-//              city = EXCLUDED.city
-//              RETURNING *`,
-//             [userId, password,user_name, lastname, address, phone, country, city]
-//         );
-
-//         res.status(200).json(profileResult.rows[0]);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
 
 app.put('/users/:email', async (req, res) => {
     const { email } = req.params;
@@ -176,7 +120,7 @@ app.put('/users/:email', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'app.js'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));//probar en vez de index.html app.js o app.jsx
 });
 
 //esta tengo que ver bien si "redirijo" a app.js o a index.html
