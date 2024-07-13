718412a4ed69a78292fe04e04daa6f30ad2038e4
Maxi S <maxi0062009@hotmail.com>
2024-07-12 23:37:51 -0300

    quito .env

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -256,7 +256,7 @@ export default function Users() {
                 </form>
             </div>
             </div>
-            {error && <p className="error">{error}</p>}
+            {/* {error && <p className="error">{error}</p>} */}
         </div>
     );
 }
